02b55cc8f37404f651d0cce826f3250395ff7b0d
Khaled Nassar <45688522+knassar702@users.noreply.github.com>
2020-04-17 14:00:03 +0200

    Update dump_emails.py

--- dump_emails.py
+++ dump_emails.py
@@ -2,11 +2,28 @@
 import re
 
 data = '''
+I Love You lol
+Khaled : 109-140-351
+Mohamed : 150-999-123
+lol : 150.999.123
+ff : 150*999-123
+sdg nokia.com
+gat
+bat
+dat
+Mr. Khaled
+Mr. Zigoo0
+Mrs. LOl
 knassar702@gmail.com
 asfasfko@lol.com
 asfokf@gmail.com
 afl260.8@lo.com
-<h3> your email is : lol@gmail.com </h3>
-<p> email : himan.607@gmail.com </p>
+<h3> your email is : lol@gmail.com
+https://google.com
+lol@ff.net
+http://nokia.com
 '''
-print(re.findall(r'[a-zA-Z0-9.-_+-]*@[a-zA-Z]+\.com',data))
+d = re.compile(r'[a-zA-Z0-9.-_+-]*@[a-zA-Z]+\.(com|net|edu|eg)')
+m = d.finditer(data)
+for i in m:
+        print(i.group())
